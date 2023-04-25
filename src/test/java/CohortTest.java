deda90648d0b0e723bb2ba339507fe6fca12f94d
Genesis Sarabia <genesissarabia14@gmail.com>
2023-04-25 14:03:25 -0500

    completed junit exercises

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,43 +1,49 @@
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CohortTest{
 
-    Cohort azeban = new Cohort();
+    Cohort azeban;
     Student genesis = new Student(4567839, "Genesis");
     Student lenny = new Student(8937499, "Isleny");
+    Student bryan = new Student(8374759, "Bryan");
 
     @Before
     public void setUp(){
-        azeban.addStudent(genesis);
+        this.azeban = new Cohort();
+
+       this.azeban.addStudent(genesis);
         genesis.addGrade(96);
         genesis.addGrade(98);
         genesis.addGrade(97);
 
-        azeban.addStudent(lenny);
+        this.azeban.addStudent(lenny);
         lenny.addGrade(95);
         lenny.addGrade(95);
         lenny.addGrade(95);
+
     }
 
     @Test
     public void testAddStudent(){
         assertNotNull(azeban);
+        azeban.addStudent(bryan);
+        assertEquals(3, azeban.getStudents().size());
     }
 
     @Test
     public void testInstanceGetStudentList(){
         assertNotNull(azeban.getStudents());
+        assertEquals("Genesis", azeban.getStudents().get(0).getName());
     }
 
-//    @Test
-//    public void testGetAverage(){
-//        assertSame(192, azeban.getCohortAverage());
-//        System.out.println(azeban.getCohortAverage());
-//        //find out if you're actually adding grades bc the debugger is saying the size of the arraylist is 0.
-//    }
+    @Test
+    public void testGetAverage(){
+        assertEquals(96.0, azeban.getCohortAverage(), 0);
+        System.out.println(azeban.getCohortAverage());
+    }
 
 
 }
